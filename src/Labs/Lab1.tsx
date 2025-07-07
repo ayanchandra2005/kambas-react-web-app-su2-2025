07b022caa324d2ee43e8afe3f3bd6a297f2435e1
ayanchandra2005 <ayanchandra2005@gmail.com>
2025-07-07 06:15:50 -0400

    A1

--- src/Labs/Lab1.tsx
+++ src/Labs/Lab1.tsx
@@ -1,7 +1,7 @@
 export default function Lab1() {
   return (
     <div id="wd-lab1">
-      <h1>Ayan Chandra | CS5610 Summer 2 2025</h1>
+      <h1>Ayan Chandra - CS5610 Summer 2 2025</h1>
       <h2>Lab 1</h2>
       <h3>HTML Examples</h3>
       <div id="wd-h-tag">
