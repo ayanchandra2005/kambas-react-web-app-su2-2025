79cccb364bb6ac57546dba15f0006256b28031e2
ayanchandra2005 <ayanchandra2005@gmail.com>
2025-07-07 05:40:31 -0400

    Assignment 1

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -102,7 +102,7 @@ export default function AssignmentEditor() {
             <input
               id="wd-available-until"
               type="date"
-              defaultValue="2025-07-08"
+              defaultValue="2025-07-10"
             />
           </td>
         </tr>
