4210b14f2fe1d931a5a64babc8d03a9068760c53
ayanchandra2005 <ayanchandra2005@gmail.com>
2025-07-30 03:04:11 -0400

    a4

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -58,7 +58,7 @@ export default function Modules() {
                   moduleId={module._id}
                   deleteModule={(moduleId) => dispatch(deleteModule(moduleId))}
                   editModule={(moduleId) => dispatch(editModule(moduleId))}
-                  saveModule={(moduleId) => dispatch(updateModule({ ...module, editing: false }))}
+                  // saveModule={(moduleId) => dispatch(updateModule({ ...module, editing: false }))}
                 />
               </div>
               {module.lessons && (
